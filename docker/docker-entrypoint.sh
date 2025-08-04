8a6f1b6c0c0e137284190e6d418433d319c9b2ad
provydon <providenceifeosame@gmail.com>
2025-08-04 13:53:26 +0100

    Make frankenphp executable in docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 touch database/database.sqlite
 php artisan migrate --force
 php artisan octane:install --server=frankenphp
+chmod +x /usr/local/bin/frankenphp
 php artisan optimize
 
 exec "$@"
